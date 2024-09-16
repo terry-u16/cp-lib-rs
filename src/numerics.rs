dd468be43b68b21382bf746fe72eb0cac18cad5c
terry-u16 <terry-u16@users.noreply.github.com>
2024-09-16 19:27:19 +0900

    エラトステネスの篩を実装

--- src/numerics.rs
+++ src/numerics.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use num::PrimInt;
 use num_traits::NumAssign;
 
@@ -107,13 +108,16 @@ impl<T: PrimInt + NumAssign> Iterator for Divisiors<T> {
 
 /// 素因数分解を行う
 ///
+/// 素因数分解を行うイテレータを返す
+/// 素因数は昇順で返される
+///
 /// # Examples
 ///
 /// ```
 /// use cp_lib_rs::numerics::prime_factorize;
 /// use itertools::*;
 ///
-/// let factors = prime_factorize(12).sorted().collect_vec();
+/// let factors = prime_factorize(12).collect_vec();
 /// assert_eq!(factors, vec![(2, 2), (3, 1)]);
 /// ```
 pub fn prime_factorize<T: PrimInt + NumAssign>(n: T) -> impl Iterator<Item = (T, usize)> {
@@ -160,6 +164,116 @@ impl<T: PrimInt + NumAssign> Iterator for PrimeFactorize<T> {
             let n = self.n;
             self.n = T::one();
             return Some((n, 1));
+        }
+
+        None
+    }
+}
+
+/// エラトステネスの篩
+///
+/// # Examples
+///
+/// ```
+/// use cp_lib_rs::numerics::Erathosthenes;
+/// use itertools::*;
+///
+/// let erathosthenes = Erathosthenes::new(1000);
+/// assert!(erathosthenes.is_prime(2));
+///
+/// let factors = erathosthenes.prime_factorize(12).collect_vec();
+/// assert_eq!(factors, vec![(2, 2), (3, 1)]);
+/// ```
+#[derive(Debug, Clone)]
+pub struct Erathosthenes {
+    max: usize,
+    smallest_prime_factor: Vec<usize>,
+}
+
+impl Erathosthenes {
+    /// エラトステネスの篩を初期化する
+    ///
+    /// # Arguments
+    ///
+    /// * `max` - 素数判定を行う最大値
+    pub fn new(max: u32) -> Self {
+        let mut smallest_prime_factor = (0..=max as usize).collect_vec();
+        let mut i = 2;
+
+        while i * i <= max as usize {
+            if smallest_prime_factor[i] == i {
+                for j in (i * 2..=max as usize).step_by(i) {
+                    if smallest_prime_factor[j] == j {
+                        smallest_prime_factor[j] = i;
+                    }
+                }
+            }
+
+            i += 1;
+        }
+
+        Self {
+            max: max as usize,
+            smallest_prime_factor,
+        }
+    }
+
+    /// 素数判定を行う
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - 素数判定を行う値
+    ///
+    /// # Examples
+    ///
+    /// ```
+    ///
+    /// use cp_lib_rs::numerics::Erathosthenes;
+    ///
+    /// let erathosthenes = Erathosthenes::new(1000);
+    ///
+    /// assert!(erathosthenes.is_prime(2));
+    /// assert!(!erathosthenes.is_prime(4));
+    pub fn is_prime(&self, n: u32) -> bool {
+        let n = n as usize;
+        assert!(n <= self.max);
+        n >= 2 && self.smallest_prime_factor[n] == n
+    }
+
+    /// 素因数分解を行う
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - 素因数分解を行う値
+    pub fn prime_factorize<'a>(&'a self, n: u32) -> ErathosthenesIter<'a> {
+        let n = n as usize;
+        assert!(n <= self.max);
+        ErathosthenesIter {
+            n,
+            erathosthenes: self,
+        }
+    }
+}
+
+pub struct ErathosthenesIter<'a> {
+    n: usize,
+    erathosthenes: &'a Erathosthenes,
+}
+
+impl<'a> Iterator for ErathosthenesIter<'a> {
+    type Item = (u32, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.n > 1 {
+            let mut count = 0;
+            let spf = self.erathosthenes.smallest_prime_factor[self.n];
+
+            while self.n > 1 && self.erathosthenes.smallest_prime_factor[self.n] == spf {
+                count += 1;
+                self.n /= spf;
+            }
+
+            return Some((spf as u32, count));
         }
 
         None
@@ -220,5 +334,29 @@ mod test {
     #[should_panic]
     fn prime_factorize_ng() {
         prime_factorize(0).count();
+    }
+
+    #[test]
+    fn erathosthenes_is_prime() {
+        let erathosthenes = Erathosthenes::new(1000);
+        assert!(erathosthenes.is_prime(2));
+        assert!(erathosthenes.is_prime(3));
+        assert!(erathosthenes.is_prime(5));
+        assert!(erathosthenes.is_prime(7));
+        assert!(erathosthenes.is_prime(11));
+        assert!(!erathosthenes.is_prime(1));
+        assert!(!erathosthenes.is_prime(4));
+        assert!(!erathosthenes.is_prime(6));
+        assert!(!erathosthenes.is_prime(8));
+        assert!(!erathosthenes.is_prime(9));
+        assert!(!erathosthenes.is_prime(10));
+    }
+
+    #[test]
+    fn erathosthenes_prime_factorize() {
+        let erathosthenes = Erathosthenes::new(1000);
+        let factors = erathosthenes.prime_factorize(12).collect_vec();
+
+        assert_eq!(factors, vec![(2, 2), (3, 1)]);
     }
 }
